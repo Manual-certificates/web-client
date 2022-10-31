a92eecfd9ab5bd8a64062cf60befd94bca3d9fab
lukachi <lukachisama@gmail.com>
2022-11-01 00:25:50 +0200

    created select field with default and custom header and options, without downshift and wai-ria

--- src/fields/SelectField.vue
+++ src/fields/SelectField.vue
@@ -1,56 +1,156 @@
+<script lang="ts" setup>
+import { Icon } from '@/common'
+
+import {
+  computed,
+  getCurrentInstance,
+  onMounted,
+  ref,
+  useAttrs,
+  watch,
+} from 'vue'
+import { useRouter } from '@/router'
+import { onClickOutside } from '@vueuse/core'
+
+const props = withDefaults(
+  defineProps<{
+    modelValue: string | number
+    valueOptions?: string[] | number[]
+    label?: string
+    placeholder?: string
+    errorMessage?: string
+  }>(),
+  {
+    valueOptions: () => [],
+    type: 'text',
+    label: '',
+    placeholder: ' ',
+    errorMessage: '',
+  },
+)
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: number | string): void
+}>()
+
+const attrs = useAttrs()
+
+const selectElement = ref<HTMLDivElement>()
+
+const isDropdownOpen = ref(false)
+const uid = getCurrentInstance()?.uid
+
+const router = useRouter()
+
+router.afterEach(() => {
+  closeDropdown()
+})
+
+const isDisabled = computed(() =>
+  ['', 'disabled', true].includes(attrs.disabled as string | boolean),
+)
+
+const isReadonly = computed(() =>
+  ['', 'readonly', true].includes(attrs.readonly as string | boolean),
+)
+
+const selectFieldClasses = computed(() => ({
+  'select-field': true,
+  'select-field--error': props.errorMessage,
+  'select-field--open': isDropdownOpen.value,
+  'select-field--disabled': isDisabled.value,
+  'select-field--readonly': isReadonly.value,
+}))
+
+const setHeightCSSVar = (element: HTMLElement) => {
+  element.style.setProperty(
+    '--field-error-msg-height',
+    `${element.scrollHeight}px`,
+  )
+}
+
+const toggleDropdown = () => {
+  isDropdownOpen.value ? closeDropdown() : openDropdown()
+}
+
+const openDropdown = () => {
+  isDropdownOpen.value = true
+}
+
+const closeDropdown = () => {
+  isDropdownOpen.value = false
+}
+
+const select = (value: string | number) => {
+  if (isDisabled.value || isReadonly.value) return
+
+  emit('update:modelValue', value)
+  closeDropdown()
+}
+
+onMounted(() => {
+  if (selectElement.value) {
+    onClickOutside(selectElement, () => {
+      closeDropdown()
+    })
+  }
+})
+
+watch(
+  () => props.modelValue,
+  () => {
+    closeDropdown()
+  },
+)
+</script>
+
 <template>
   <div :class="selectFieldClasses">
+    <label
+      v-if="label"
+      class="select-field__label"
+      :for="`select-field--${uid}`"
+    >
+      {{ label }}
+    </label>
     <div ref="selectElement" class="select-field__select-wrp">
-      <div class="select-field__select-head-wrp">
-        <button
-          type="button"
-          class="select-field__select-head"
-          @click="toggleDropdown"
-        >
-          <template v-if="$slots.head && !!modelValue">
-            <slot
-              name="head"
-              :select-field="{
-                select,
-                isOpen: isDropdownOpen,
-                close: closeDropdown,
-                open: openDropdown,
-                toggle: toggleDropdown,
-              }"
-            />
+      <button
+        type="button"
+        class="select-field__select-head"
+        @click="toggleDropdown"
+      >
+        <template v-if="$slots.head">
+          <slot
+            name="head"
+            :select-field="{
+              select,
+              isOpen: isDropdownOpen,
+              close: closeDropdown,
+              open: openDropdown,
+              toggle: toggleDropdown,
+            }"
+          />
+        </template>
+        <template v-else>
+          <template v-if="modelValue">
+            {{ modelValue }}
           </template>
           <template v-else>
-            <template v-if="modelValue">
-              {{ modelValue }}
-            </template>
-            <template v-else-if="!label">
-              <span class="select-field__placeholder">
-                {{ props.placeholder }}
-              </span>
-            </template>
+            <span class="select-field__placeholder">
+              {{ props.placeholder }}
+            </span>
           </template>
-          <icon
-            :class="[
-              'select-field__select-head-indicator',
-              {
-                'select-field__select-head-indicator--open': isDropdownOpen,
-              },
-            ]"
-            :name="$icons.chevronDown"
-          />
-        </button>
-        <span
-          class="select-field__focus-indicator"
-          v-if="scheme === 'secondary'"
+        </template>
+        <icon
+          :class="[
+            'select-field__select-head-indicator',
+            {
+              'select-field__select-head-indicator--open': isDropdownOpen,
+            },
+          ]"
+          :name="$icons.chevronDown"
         />
-        <label
-          v-if="label"
-          class="select-field__label"
-          :for="`select-field--${uid}`"
-        >
-          {{ label }}
-        </label>
-      </div>
+      </button>
       <transition name="select-field__select-dropdown">
         <div v-if="isDropdownOpen" class="select-field__select-dropdown">
           <template v-if="$slots.default">
@@ -96,124 +196,8 @@
   </div>
 </template>
 
-<script lang="ts" setup>
-import { Icon } from '@/common'
-
-import {
-  computed,
-  getCurrentInstance,
-  onMounted,
-  ref,
-  useAttrs,
-  watch,
-} from 'vue'
-import { useRouter } from '@/router'
-import { onClickOutside } from '@vueuse/core'
-
-type SCHEMES = 'primary' | 'secondary'
-
-const props = withDefaults(
-  defineProps<{
-    scheme?: SCHEMES
-    modelValue: string | number
-    valueOptions?: string[] | number[]
-    label?: string
-    placeholder?: string
-    errorMessage?: string
-  }>(),
-  {
-    scheme: 'primary',
-    valueOptions: () => [],
-    type: 'text',
-    label: '',
-    placeholder: ' ',
-    errorMessage: '',
-  },
-)
-
-const emit = defineEmits<{
-  (e: 'update:modelValue', value: number | string): void
-}>()
-
-const attrs = useAttrs()
-
-const selectElement = ref<HTMLDivElement>()
-
-const isDropdownOpen = ref(false)
-const uid = getCurrentInstance()?.uid
-
-const router = useRouter()
-
-router.afterEach(() => {
-  closeDropdown()
-})
-
-const isDisabled = computed(() =>
-  ['', 'disabled', true].includes(attrs.disabled as string | boolean),
-)
-
-const isReadonly = computed(() =>
-  ['', 'readonly', true].includes(attrs.readonly as string | boolean),
-)
-
-const isLabelActive = computed(() => isDropdownOpen.value || !!props.modelValue)
-
-const selectFieldClasses = computed(() => ({
-  'select-field': true,
-  'select-field--error': props.errorMessage,
-  'select-field--open': isDropdownOpen.value,
-  'select-field--disabled': isDisabled.value,
-  'select-field--readonly': isReadonly.value,
-  'select-field--label-active': isLabelActive.value,
-  [`select-field--${props.scheme}`]: true,
-}))
-
-const setHeightCSSVar = (element: HTMLElement) => {
-  element.style.setProperty(
-    '--field-error-msg-height',
-    `${element.scrollHeight}px`,
-  )
-}
-
-const toggleDropdown = () => {
-  isDropdownOpen.value ? closeDropdown() : openDropdown()
-}
-
-const openDropdown = () => {
-  if (isDisabled.value || isReadonly.value) return
-
-  isDropdownOpen.value = true
-}
-
-const closeDropdown = () => {
-  isDropdownOpen.value = false
-}
-
-const select = (value: string | number) => {
-  if (isDisabled.value || isReadonly.value) return
-
-  emit('update:modelValue', value)
-  closeDropdown()
-}
-
-onMounted(() => {
-  if (selectElement.value) {
-    onClickOutside(selectElement, () => {
-      closeDropdown()
-    })
-  }
-})
-
-watch(
-  () => props.modelValue,
-  () => {
-    closeDropdown()
-  },
-)
-</script>
-
 <style lang="scss" scoped>
-$z-local-index: 2;
+$z-local-index: 1;
 
 .select-field {
   display: flex;
@@ -225,7 +209,6 @@ $z-local-index: 2;
   &--disabled,
   &--readonly {
     opacity: 0.5;
-    pointer-events: none;
   }
 }
 
@@ -233,44 +216,11 @@ $z-local-index: 2;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  pointer-events: none;
-  position: absolute;
-  padding: toRem(4);
-  top: 50%;
-  left: var(--field-padding-left);
-  transform: translateY(-50%);
-  color: var(--field-label);
-  font-size: toRem(16);
-  font-weight: 400;
-  line-height: 1.3;
-  background: var(--field-bg-primary);
 
   @include field-label;
 
-  transition-property: all;
-
-  .select-field--secondary & {
-    background: none;
-    padding: 0;
-  }
-
   .select-field--error & {
     color: var(--field-error);
-  }
-
-  .select-field--label-active & {
-    top: 0;
-    font-size: toRem(12);
-    line-height: 1.3;
-    font-weight: 700;
-  }
-
-  .select-field--open & {
-    color: var(--primary-main);
-  }
-
-  .select-field--label-active.select-field--secondary & {
-    transform: translateY(50%);
   }
 }
 
@@ -280,102 +230,26 @@ $z-local-index: 2;
   position: relative;
 }
 
-.select-field__select-head-wrp {
-  position: relative;
-  width: 100%;
-  height: 100%;
-}
-
 .select-field__select-head {
-  background: var(--field-bg-primary);
   padding: var(--field-padding);
   padding-right: calc(var(--field-padding-right) + #{toRem(24)});
+  transition-property: box-shadow;
   text-align: left;
   width: 100%;
   height: 100%;
 
-  $field-text-height: calc(
-    var(--field-text-font-size) * var(--field-text-line-height)
-  );
-
-  min-height: calc(
-    $field-text-height + var(--field-padding-top) + var(--field-padding-bottom)
-  );
+  @include field-border;
 
   @include field-text;
 
-  transition-property: all;
-
-  & + .select-field__focus-indicator {
-    pointer-events: none;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-
-    &:after {
-      content: '';
-      position: absolute;
-      bottom: toRem(-2);
-      left: 50%;
-      transform: translateX(-50%);
-      height: toRem(2);
-      width: 0;
-      background: var(--primary-main);
-      transition: width calc(var(--field-transition-duration) + 0.3s);
-
-      .select-field--error & {
-        background: var(--field-error);
-      }
-    }
-  }
-
-  .select-field--primary & {
-    @include field-border;
-  }
-
-  .select-field--secondary & {
-    position: relative;
-    background: var(--background-secondary-main);
-    box-shadow: inset 0 0 0 toRem(50) var(--field-bg-secondary),
-      0 toRem(2) 0 0 var(--field-border);
-    padding: calc(var(--field-padding-top) + #{toRem(12)})
-      var(--field-padding-right) var(--field-padding-bottom)
-      var(--field-padding-left);
-  }
-
-  .select-field--error.select-field--primary & {
-    box-shadow: inset 0 0 0 toRem(50) var(--field-bg-primary),
-      0 0 0 toRem(1) var(--field-error);
+  .select-field--error & {
     border-color: var(--field-error);
-  }
-
-  .select-field--error.select-field--secondary & {
-    box-shadow: inset 0 0 0 toRem(50) var(--field-bg-secondary),
-      0 toRem(2) 0 0 var(--field-error);
-  }
-
-  .select-field--open.select-field--primary & {
-    box-shadow: inset 0 0 0 toRem(50) var(--field-bg-primary),
-      0 0 0 toRem(2) var(--primary-main);
-    border-color: var(--primary-main);
-  }
-
-  .select-field--open.select-field--secondary & {
-    & + .select-field__focus-indicator {
-      &:after {
-        width: 100%;
-      }
-    }
   }
 }
 
 .select-field__placeholder {
   font: inherit;
   opacity: 0.25;
-
-  @include field-placeholder;
 }
 
 .select-field__select-head-indicator {
@@ -399,15 +273,14 @@ $z-local-index: 2;
   flex-direction: column;
   position: absolute;
   overflow: hidden auto;
-  top: 105%;
+  top: 0;
   right: 0;
   width: 100%;
   max-height: 500%;
   z-index: $z-local-index;
-  background: var(--field-bg-secondary);
-  box-shadow: 0 toRem(1) toRem(2) rgba(var(--black-rgb), 0.3),
-    0 toRem(2) toRem(6) toRem(2) rgba(var(--black-rgb), 0.15);
-  border-radius: toRem(4);
+  background: var(--field-bg);
+
+  @include field-border;
 }
 
 .select-field__select-dropdown-enter-active {
@@ -422,24 +295,23 @@ $z-local-index: 2;
   from {
     opacity: 0;
     transform: translateY(20%);
-    max-height: 0;
+    height: 0;
   }
 
   to {
     opacity: 1;
     transform: translateY(0);
-    max-height: 500%;
+    height: 500%;
   }
 }
 
 .select-field__select-dropdown-item {
   text-align: left;
   width: 100%;
-  padding: toRem(8) var(--field-padding-right) toRem(8)
-    var(--field-padding-left);
+  padding: var(--field-padding);
 
   &:hover {
-    background: rgba(var(--primary-dark-rgb), 0.15);
+    background: rgba(var(--primary-main-rgb), 0.05);
   }
 
   &--active {
